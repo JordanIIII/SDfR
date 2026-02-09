2f56fe482acf8c6246403bbdb155d96879012096
Jordan Nwandu <jordanvelserbroek@gmail.com>
2026-02-09 12:00:27 +0100

    Assignment 1.1 completed
    Included README.txt and headers in each file.

---
+++ assignment1/assignment1.1/README.txt
@@ -0,0 +1,11 @@
+Assignment 1.1
+−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
+Ensure directory is set to .\assignment1.1
+−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
+1. Compile commands:
+g++ -c .\src\main.cpp -o main.o
+g++ -c .\src\divide.cpp -o divide.o
+2. Link + compile command:
+g++ .\main.o .\divide.o -o assignment1.1.exe
+3. Run command:
+.\assignment1.1.exe
--- assignment1/assignment1.1/include/divide.hpp
+++ assignment1/assignment1.1/include/divide.hpp
@@ -1,3 +1,12 @@
+//=============================================================================
+// Authors : Jordan Nwandu (s3537544) and Steyn Fokkema (s2710714)
+// Group : PPD 1  
+// License : LGPL open source license
+//
+// Brief : Simple division function that divides two integers and returns the result.
+//
+//=============================================================================
+
 #ifndef DIVIDE_HPP
 #define DIVIDE_HPP
 
