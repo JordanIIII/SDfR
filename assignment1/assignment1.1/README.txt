92f51aff3e4364a2244e5147a1ae13737720d28d
Jordan Nwandu <jordanvelserbroek@gmail.com>
2026-02-09 12:07:40 +0100

    Updated/better compile command

--- assignment1/assignment1.1/README.txt
+++ assignment1/assignment1.1/README.txt
@@ -2,9 +2,8 @@ Assignment 1.1
 −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
 Ensure directory is set to .\assignment1.1
 −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
-1. Compile commands:
-g++ -c .\src\main.cpp -o main.o
-g++ -c .\src\divide.cpp -o divide.o
+1. Compile command:
+g++ -c src\main.cpp src\divide.cpp -Iinclude
 2. Link + compile command:
 g++ .\main.o .\divide.o -o assignment1.1.exe
 3. Run command:
---
+++ assignment1/assignment1.2/src/main.cpp
@@ -0,0 +1,22 @@
+//=============================================================================
+// Authors : Jordan Nwandu (s3537544) and Steyn Fokkema (s2710714)
+// Group   : PPD 1
+// License : LGPL open source license
+//
+// Brief   : BMI Calculator
+//
+//=============================================================================
+
+#include <iostream>
+
+int main() {
+    double weight, height, bmi;
+
+    std :: cout << "Enter your weight in kg: ";
+    std :: cin >> weight;
+    std :: cout << "Enter your height in meters: ";
+    std :: cin >> height;
+
+    bmi = weight / (height * height);
+    std :: cout << "Your BMI is: " << bmi << std :: endl;
+}
