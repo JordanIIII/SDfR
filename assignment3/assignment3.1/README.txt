9b41f89e0cfc6cc5001086712a2d8801660ae598
Jordan Nwandu <jordanvelserbroek@gmail.com>
2026-02-20 11:08:46 +0100

    required files added

---
+++ assignment3/assignment3.1/README.txt
@@ -0,0 +1,10 @@
+Assignment 3.1
+−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
+Ensure directory is set to .\assignment3.1
+−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
+1. Compile command:
+    g++ Enter the compile command here
+2. Run command:
+    Enter the run command here
+3. Usage:
+    Enter usage instructions here
--- assignment3/assignment3.1/src/main.cpp
+++ assignment3/assignment3.1/src/main.cpp
@@ -1,3 +1,13 @@
+//=============================================================================
+// Authors : Jordan Nwandu (s3537544) and Steyn Fokkema (s2710714)
+// Group   : PPD 1
+// License : LGPL open source license
 //
-// Created by Jordan on 16-2-2026.
+// Brief   : Enter a brief description here
 //
+//=============================================================================
+
+
+int main() {    
+    return 0;
+}
