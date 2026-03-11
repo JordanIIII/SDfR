03f02f6b80a67df0e76fab36a2a93d4b5bce3d6f
Jordan Nwandu <jordanvelserbroek@gmail.com>
2026-03-11 09:40:28 +0100

    Set all input parameters to the same value

--- assignment4/assignment4.1/src/main.cpp
+++ assignment4/assignment4.1/src/main.cpp
@@ -18,14 +18,14 @@
 
 int main() {
     // Parameters: (senderName, senderAddress, receiverName, receiverAddress, weight, flatFee, weightFee)
-    twoDayPackage twoDay("Richi", "Mandarijn Straat", "Vlad", "Lekker Gezellig Straat", 30.0, 5.0, 2.5);
-    sameDayPackage sameDay("Jordan", "Calslaan", "Steyn", "Matenweg", 5.0, 2.0, 5.0);
-    overnightPackage overnight("Tomoki", "123 Main St", "Sjoerd", "456 Elm St", 15.0, 5.0, 2.5);
-    internationalPackage international("Ira", "Teletubbieland", "Malta", "742 Evergreen Terrace", 7.0, 30.0, 3);
+    twoDayPackage twoDay("Richi", "Mandarijn Straat", "Vlad", "Lekker Gezellig Straat", 10.0, 5.0, 2.5);
+    sameDayPackage sameDay("Jordan", "Calslaan", "Steyn", "Matenweg", 10.0, 5.0, 2.5);
+    overnightPackage overnight("Tomoki", "123 Main St", "Sjoerd", "456 Elm St", 10.0, 5.0, 2.5);
+    internationalPackage international("Ira", "Teletubbieland", "Malta", "742 Evergreen Terrace", 10.0, 5.0, 2.5);
     overnightInternationalPackage overnightInternational("Alice", "Wonderland", "Bob", "Builder Street", 10.0, 5.0, 2.5);
     
     // Parameters: (senderName, senderAddress, receiverName, receiverAddress, weight, flatFee, weightFee, numberOfReturns, orderNumber)
-    retourPackage retour("Charlie", "123 Oak St", "David", "456 Pine St", 20.0, 5.0, 2.5, 2, 2026001); 
+    retourPackage retour("Charlie", "123 Oak St", "David", "456 Pine St", 10.0, 5.0, 2.5, 2, 2026001); 
 
 
     // same info and cost print function for every package type
