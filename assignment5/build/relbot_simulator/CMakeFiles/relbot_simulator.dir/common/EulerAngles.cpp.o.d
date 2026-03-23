6ede346d2edbd5ae1c8a1c668d18d6dcd59fdda5
Jordan <jordan@example.com>
2026-03-23 17:06:05 +0100

    Deleted build, install, and log folders (less clutter)

--- assignment5/build/relbot_simulator/CMakeFiles/relbot_simulator.dir/common/EulerAngles.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/relbot_simulator.dir/common/EulerAngles.cpp.o: \
- /home/jordan/SDfR/assignment5/relbot_simulator/common/EulerAngles.cpp \
- /usr/include/stdc-predef.h
--- assignment5/build/relbot_simulator/CMakeFiles/relbot_simulator.dir/common/MotionProfiles.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/relbot_simulator.dir/common/MotionProfiles.cpp.o: \
- /home/jordan/SDfR/assignment5/relbot_simulator/common/MotionProfiles.cpp \
- /usr/include/stdc-predef.h
